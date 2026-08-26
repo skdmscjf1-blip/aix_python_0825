c742bd3c336a2641614403a36fad8f82cb0baeed
skdmscjf1@naver.com <skdmscjf1@naver.com>
2026-08-26 17:54:38 +0900

    개인겅부

--- P0826/psafsaf_09.py
+++ P0826/psafsaf_09.py
@@ -1,8 +1,17 @@
-score = int(input("점수를 입력해주세요 : "))
+# score = int(input("점수를 입력해주세요 : "))
 
-if score >= 90 :
-	print("A등급")
-elif score >= 80:
-	print("B등급")
-else :
-	print("C등급")
+# if score >= 90 :
+# 	print("A등급")
+# elif score >= 80:
+# 	print("B등급")
+# else :
+# 	print("C등급")
+
+
+# age = int(input("나이를 입력하세요"))
+# test = int(input("시험점수를 입력하세요"))
+
+# if age >= 20 and test >= 80 : 
+# 	print("합격")
+# else : 
+#     print("불합격")
