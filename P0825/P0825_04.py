62ab04a33782f3496a022ef01d99f0840d9f2d45
skdmscjf1@naver.com <skdmscjf1@naver.com>
2026-08-26 10:22:07 +0900

    파이썬수업01-print,변수,타입

---
+++ P0825/P0825_04.py
@@ -0,0 +1,46 @@
+# 타입 확인
+
+a = 100
+b = 10.1
+c = "안녕"
+d = True
+#c가 무슨타입?
+#타입 확인방법 type(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+# a = 9
+# b = 4
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)    #몫 2
+# print(a%b)     #나머지 1
+# print(a**b)    #제곱 6561
+
+# print(9+4)
+# print(9-4)
+# print(9*4)
+# print(9/4)
+# print(9//4)    #몫 2
+# print(9%4)     #나머지 1
+# print(9**4)    #제곱 6561
+
+
+
+# 변수 :  값을 저장하는 공간, 타입은 값을 입력할때 정해짐.
+# 타입 : 4개 숫자형타입(정수타입,실수타입),문자열타입,불타입
+# a = 10      # 숫자형타입 - 정수타입
+# b = 10.1    # 숫자형타입 - 실수타입, 소수점 
+# aa = "안녕"  # 문자열타입
+# abc = True  # 불타입(bool) - True,False / boolean
+
+# 예약어는 변수로 사용할수 없음.
+# True = 1
+# print = 5
---
+++ P0825/test.py
@@ -0,0 +1,9 @@
+name = "영희"
+age = 32
+weight = 55.5
+filates_member = True
+
+print(name)
+print(age)
+print(weight)
+print(filates_member)
