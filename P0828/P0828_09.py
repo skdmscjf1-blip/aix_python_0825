4c2e090e98f11f770d28c94da5f494eea06eb27b
skdmscjf1@naver.com <skdmscjf1@naver.com>
2026-08-28 17:59:56 +0900

    for문

---
+++ P0828/P0828_09.py
@@ -0,0 +1,72 @@
+
+# for i in range(2,10) :
+#     for j in range(1,10) :
+#          print(f"{i} X {j} = {i*j}")
+
+# # 번호표 001
+# for i in range (0,10) :
+#      for j in range (0,10) :
+#           for k in range(0,10) :
+#                print("{}{}{}".format(i,j,k))
+
+
+# for i in range (0,10) :
+#     for j in range (0,10) :
+#         for k in range(0,10) :
+#                 print((i*100)+(j*10)+k+1,":",i,j,k)
+
+
+
+# for i in range(1,10):
+#     print(f"2 X {i} = {2*i}")
+
+# sum = 0
+# for i in range(1,11):
+#     sum = sum+i
+# print("합계 :",sum)
+
+# 합계가 100이 넘가는 시점은 i가 얼마일때?
+
+# sum = 0
+# for i in range(1,11) :
+#     sum = sum+i
+#     if sum >=11 :
+#         print("10보다 크기 바로앞일때:",i-1)
+#         print("10초과전 시점 : ",sum-i)
+#         break
+
+
+# for i in range(3):
+#     no = i+1
+#     name = input("이름입력 : ")
+#     kor = int(input("국어점수 입력 : "))
+#     print("{}\t{}\t{}".format(no,name,kor))
+
+
+# for i in range(10):
+#     print("안녕")
+
+# for _ in range(10):
+#     print("안녕")
+
+
+
+# # for 변수 in 범위:
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,5):  
+#     print(i*10)  
+
+# for i in range(0,10,2):
+#     print(i)  
+
+
+# for i in [1,5,3,2]:
+#     print(i)  
+
+# for i in "안녕하세요":
+#     print(i) 
+
+# arr = list(range(1,11))          
+# print(arr)
