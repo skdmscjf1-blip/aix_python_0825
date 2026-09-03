1e692f1c8381e5780561002b4b487a2f09305c11
skdmscjf1@naver.com <skdmscjf1@naver.com>
2026-09-03 09:26:14 +0900

    바꾸자

--- P0902/P0902_01.py
+++ P0902/P0902_01.py
@@ -1,18 +1,23 @@
 
-# alist = list(range(1,26))
-# #print(alist)
+alist = list(range(1,26))
+#print(alist)
 
-# for i in range(len(alist)) :
-#     if (i+1)%5!=0 :
-#         print (alist[i],end="\t")
-#     else :
-#         print(alist[i])
+for i in range(len(alist)) :
+    if (i+1)%5!=0 :
+        print (alist[i],end="\t")
+    else :
+        print(alist[i])
+# 1       2       3       4       5
+# 6       7       8       9       10
+# 11      12      13      14      15
+# 16      17      18      19      20
+# 21      22      23      24      25
 
 
 
 
-# #1,25까지 리스트를 생성하고 
-# #랜덤으로 리스트를 섞음 다음, 5개씩 2차원 리스트를 만드시오.
+# # #1,25까지 리스트를 생성하고 
+# # #랜덤으로 리스트를 섞음 다음, 5개씩 2차원 리스트를 만드시오.
 # import random
 
 # alist = list(range(1,26))
@@ -21,18 +26,18 @@
 
 # for i in range(0,len(alist),5) :
 #         alist2.append(alist[i:i+5])
-# print(alist2)
+# print(alist2)#숫자 랜덤 
+#[[11, 22, 3, 20, 18], [6, 5, 10, 1, 17], [7, 8, 12, 15, 16], [13, 4, 25, 2, 9], [19, 14, 24, 23, 21]]
 
 
 # #문자열 3자리씩 끊어서 리스트로 작성하시오.
 # aa= "abcdefabcdefabcdefabcdefabcdef"
 # aa2 = []
-
 # for i in range(0,len(aa),3) :
 #     aa2.append(aa[i:i+3]) #0,1,2
-# print(aa2)
+# print(aa2) # ['abc', 'def', 'abc', 'def', 'abc', 'def', 'abc', 'def', 'abc', 'def']
 
-# #1차원리스트를 2차원형태로 구성
+#1차원리스트를 2차원형태로 구성
 # arr = [1,2,3,4,5,6,7,8,9]
 # arr2 = []
 # # arr2 =[
@@ -41,4 +46,4 @@
 
 # for i in range(0,len(arr),3) :
 #     arr2.append(arr[i:i+3]) #0,1,2
-# print(arr2)
+# print(arr2) # [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
