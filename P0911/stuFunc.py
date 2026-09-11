052d3d6b3b6886dd1ad447d416ae4de6c1e78f04
skdmscjf1@naver.com <skdmscjf1@naver.com>
2026-09-11 18:09:17 +0900

    잘하자

---
+++ P0911/stuFunc.py
@@ -0,0 +1,73 @@
+stu_list=[]
+stuNum = 1
+
+def main_screen() :
+    
+        print("[학생성적프로그램]")
+        print("1. 학생성적입력")
+        print("2. 학생성적출력")
+        print("3. 학생성적수정")
+        print("9. 성적파일저장")
+        print("0. 프로그램종료")
+        print("-"*60)
+
+        choice = int(input("원하는 번호 입력 : "))
+        return choice
+
+def stu_input() :
+    while True :
+        global stuNum
+        print()
+        print("[학생성적입력]")
+        no = stuNum
+        name = input(f"{stuNum}번째학생 이름입력 (0.이전페이지): ")
+        if name == "0": break
+        kor = int(input("국어점수 : "))
+        eng = int(input("영어점수 : "))
+        math = int(input("수학점수 : "))
+        total = kor+eng+math
+        avg = total / 3
+        stu_list.append({"no":no,"name":name,"kor":kor,"eng":eng,"math":math,"total":total,"avg":avg})
+        print(f"{stuNum}번째 {name}학생 성적 입력완료되었습니다.")
+        stuNum += 1
+
+def stu_output() :
+    print()
+    print("[학생성적출력]")
+    print("-"*60)
+    print("번호\t이름\t국어\t영어\t수학\t합계\t평균")
+    print("-"*60)
+    for s in stu_list :
+        print(f"{s['no']}\t{s['name']}\t{s['kor']}\t{s['eng']}\t{s['math']}\t{s['total']}\t{s['avg']:.2f}")
+
+def stu_updata () :
+    print()
+    print("[학생성적수정]")
+    name = input("수정할 학생이름 입력 : ")
+    temp = 0
+    for s in stu_list : 
+        if name == s["name"] :
+            temp = 1
+            print(f"{name} 학생을 찾았습니다.")
+            print("1국어  2.영어  3.수학")
+            a=int(input("수정할 번호 입력 (0.취소) : "))
+            if a == 0: break
+            if a==1 :
+                print("국어점수 변경")
+                print(f"현재 국어점수: {s["kor"]}")
+                s["kor"] = int(input("변경할점수 입력: "))
+                
+            elif a==2 :
+                print(f"현재 영어점수: {s["eng"]}")
+                s["eng"] = int(input("변경할점수 입력: "))
+            
+            elif a==3 :
+                print(f"현재 수학점수: {s["math"]}")
+                s["math"] = int(input("변경할점수 입력: "))
+
+        s["total"] = s["kor"] + s["eng"] + s["math"]
+        s["avg"] = s["total"] / 3
+    print("수정이 완료되었습니다.")
+    print()
+    if temp==0:
+        print(f"{name}학생 없음. 다시 입력하세요")  
